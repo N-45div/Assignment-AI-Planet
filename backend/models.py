594871de63c45225f140950287fa5f595bb0c0dc
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2024-11-15 19:55:54 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -2,9 +2,7 @@ import os
 from sqlalchemy import create_engine,Column,Integer,String,Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-load_dotenv()
 
 
 Base = declarative_base()
@@ -15,7 +13,7 @@ class PDFDocument(Base):
     title = Column(String,index=True)
     content = Column(Text,nullable = True)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush=False,bind=engine)
 Base.metadata.create_all(bind = engine)
